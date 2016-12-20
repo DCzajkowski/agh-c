5188ee98190f30eee711a70d1452bc019ed20fbb
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2016-12-20 13:52:35 +0100

    First program works with 2 functions. 2 more to go

--- 9/1.c
+++ 9/1.c
@@ -1,9 +1,5 @@
 /**
  * Napisz program zawierający następujące funkcje:
- *     int** createMatrix(int n);
- *         która tworzy macierz o wymiarze nxn. Fukcja zwraca utworzoną tablicę.
- *         W funkcji każdy wiersz ma być alokowany osobno. W funkcji należy użyć funkcji deleteMatrix().
- *
  *     void fillMatrix(int** tab, int n);
  *         która otrzymuje macierz tab o wymiarze "nxn", a następnie wypełnia ją kolejnymi
  *         liczbami naturalnymi począwszy od 1.
@@ -12,10 +8,6 @@
  *         która tworzy tablicę "dest", kopiuje do niej zawartość tablicy "src" i "wrzuca"
  *         "dest" pod przekazany adres. Obie tablice są wymiaru "nxn".
  *         W funkcji należy użyć funkcji CreateMatrix().
- *
- *     void deleteMatrix(int*** ptab, int n);
- *         która otrzymuje poprzez adres macierz o wymiarze "nxn" utworzoną przez createMatrix()
- *         i następnie zwalnia zajmowaną przez macierz pamięć, a wskaźnik reprezentujący tablicę ustawia na NULL.
  *
  * W programie należy użyć wszystkich wymienionych powyżej funkcji do utworzenia tablicy o
  * wymyslonym rozmiarze, wypełnienia jej, skopiowania do drugiej tablicy, wypisania drugiej
@@ -27,29 +19,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* createMatrix(int n) {
-    int *matrix = malloc(n * n * sizeof(int));
+void deleteMatrix(int*** ptab, int n) {
+    for (int i = 0; i < n; i++) {
+        free((*ptab)[i]);
+    }
+    free(*ptab);
+
+    *ptab = NULL;
+}
+
+int** createMatrix(int n) {
+    int **tab = malloc(n * sizeof(int*));
 
     for (int i = 0; i < n; i++) {
+        tab[i] = malloc(n * sizeof(int));
+    }
+
+    // tab[i][j] = *(*(tab+i)+j)
+    for (int i = 0; i <  n; i++) {
         for (int j = 0; j < n; j++) {
-            *(matrix + i * n + j) = 0;
+            tab[i][j] = 0;
         }
     }
 
-    return matrix;
+    return tab;
 }
 
 // void fillMatrix(int** tab, int n);
 // void copyMatrix(int*** pdest, int** src, int n);
-// void deleteMatrix(int*** ptab, int n);
 
 int main() {
     int n = 10;
-    int* matrix = createMatrix(n);
+    int** matrix = createMatrix(n);
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            printf("%2d ", *(matrix + i * n + j));
+            printf("%2d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("-----\n");
+    deleteMatrix(&matrix, n);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%2d ", matrix[i][j]);
         }
         printf("\n");
     }
